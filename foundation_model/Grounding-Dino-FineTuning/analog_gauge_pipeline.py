c61b79ee2186a4acdaf33019c9db30735390a2bc
sy.ha <sy.ha@crefle.com>
2025-08-06 19:40:04 +0900

    clockwise

--- foundation_model/Grounding-Dino-FineTuning/analog_gauge_pipeline.py
+++ foundation_model/Grounding-Dino-FineTuning/analog_gauge_pipeline.py
@@ -370,9 +370,18 @@ class AnalogGaugeInspector:
             needle_relative_angle = needle_angle_unwrapped - angle1_unwrapped
             ratio = needle_relative_angle / angle_range
             estimated_value = value1 + ratio * (value2 - value1)
-            ######### 예상 범위를 벗어난 경우 -1로 처리
-            if estimated_value < self.params.min_value or estimated_value > self.params.max_value:
-                estimated_value = -1
+
+            ######### 흐름 방향 확인 (시계방향인지 반시계방향 지)
+            sorted_values_by_angle = sorted(value_angles, key=lambda va: va[1])
+            is_clockwise = sorted_values_by_angle[0][0] < sorted_values_by_angle[-1][0]
+
+            if (is_clockwise and value1 > value2) or (not is_clockwise and value1 < value2):
+                print("Gauge flow mismatch detected. Returning 0.")
+                estimated_value = 0.0
+
+            ######### 예상 범위를 벗어난 경우 0으로 처리
+            # if estimated_value < self.params.min_value or estimated_value > self.params.max_value:
+            #     estimated_value = 0.0
 
             print(f"Estimated gauge value: {estimated_value:.3f}")
             cv2.putText(cropped_image_np_vis, f"{estimated_value:.1f}", (30, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 2)
